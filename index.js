30e297f3d862506f80f0baad91f0d7e4f533a86f
WilliamCowden <williamtye03@gmail.com>
2025-06-27 13:34:11 -0500

    Added blog DB request and populate fields with DB data

--- index.js
+++ index.js
@@ -9,51 +9,63 @@ import { fileURLToPath } from "url";
 const port = 3000;
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "world",
-  password: "1234",
-  port: 5432
-});
 let posts = [];
+let currentPost = {};
+let errorMessage;
 
+async function newQuery(queryString) {
+  const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "world",
+    password: "1234",
+    port: 5432,
+  });
+  try {
+    await db.connect();
+    console.log("DB_CONNECTION: connected");
+    const result = await db.query(queryString);
+    console.log("DB_CONNECTION: query successful");
+    await db.end();
+    console.log("DB_CONNECTION: connection ended");
+    return result;
+
+  } catch (err) {
+    errorMessage = err;
+    console.error("DB_CONNECTION: error! : ", err);
+    res.redirect("/error");
+  }
+}
+
+// middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-db.connect(console.log("DB CONNECTION SUCCESS"));
-db.query("SELECT * FROM blog_posts", (err, result) => {
-  if (err) {
-    console.error("DB QUERY ERROR: ", err);
-    res.redirect("/error");
-  } else {
-    posts = result.rows;
-    console.log("QUERY SUCCESS: ", posts[0].title);
-  }
-  db.end(console.log("DB CONNECTION ENDED"));
-});
 
-
-// routes
+// route handlers
 app.get("/", (req, res) => {
   const date = new Date();
 
-  res.render(__dirname + "/views/home.ejs", res.locals = {
+  res.render(__dirname + "/views/index.ejs", res.locals = {
     dir: __dirname,
     year: date.getFullYear(),
   });
 });
 
-app.get("/blog", (req, res) => {
+app.get("/blog", async (req, res) => {
   try {
     const date = new Date();
 
+    posts = await newQuery("SELECT * FROM tye_blog");
+    currentPost = posts.rows[0];
+console.log(posts);
     res.render(
       __dirname + "/views/blog.ejs",
       res.locals = {
         dir: __dirname,
         year: date.getFullYear(),
-        posts: posts
+        posts: posts.rows,
+        currentPost: currentPost
       }
     );
   } catch (err) {
@@ -62,13 +74,13 @@ app.get("/blog", (req, res) => {
   }
 });
 
-app.post("/blog/get-blog-post", (req, res) => {
-  
+app.post("/blog/get-post", (req, res) => {
+
   try {
     posts.forEach(post => {
       if (post.title == req.body.title) {
-        res.render(__dirname + "/views/post.ejs", 
-          res.locals = { dir: __dirname, post: post });
+        currentPost = post;
+        res.redirect("/blog");
       }
     });
   } catch (err) {
